089bd002470453854079f9d81fd464ecafc48062
D3press3d <anthonyrosman20@gmail.com>
2024-03-29 23:15:27 -0400

    chore: add global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,32 @@
+import data from '@/mocks/agents.json';
+import { AgentCard } from '../components/agentCard/AgentCard';
 
 export default function Home() {
   return (
-    <h1>hola Lindsi</h1>
+    <>
+      <h1>hola Lindsi</h1>
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
+        {data.data.map(value => {
+          return (
+            <div
+              style={{
+                margin: '80px',
+              }}
+            >
+              <AgentCard
+                name={value.displayName}
+                country={value.displayName}
+                agentBackgroundImg={value.fullPortraitV2}
+                background={value.background}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
